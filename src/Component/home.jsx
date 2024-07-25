3fee180bfb70bcc77793200491bba33c6ac6abd7
Mortalsinner <themortalsinner@gmail.com>
2024-07-25 23:42:17 +0700

    fix a few things

--- src/Component/home.jsx
+++ src/Component/home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="App">
         <header className="App-header">
         <h1> Cek Kodam Kamu</h1> 
-        <input type="text" placeholder="Masukan Nama kamu" className="input input-bordered w-full max-w-xs mt-2" />
+        <input type="text" placeholder="Masukan Nama kamu" className="input input-bordered w-full max-w-xs mt-2" /><br />
         <Button className="btn btn-primary mt-3" onClick={picker}>Submit</Button>
         {randomItem && <p className="mt-3">Kodam kamu adalah: {randomItem}</p>}
         </header>
