87c619049da8a8799b4990ce8209566a8388a7ef
Mortalsinner <themortalsinner@gmail.com>
2024-07-25 23:12:37 +0700

    DaisyUI and Tailwindcss configuration, fully fucntioning app

--- src/Component/home.jsx
+++ src/Component/home.jsx
@@ -1,25 +1,25 @@
-import { Theme, Button } from 'react-daisyui'
+import React, { useState } from 'react';
+import { Button } from 'react-daisyui';
+
 const Home = () => {
+    
+    const kodam = ['macan putih', 'sanca bogor', 'elang botak', 'Hiu Madura'];
+    const [randomItem, setRandomItem] = useState('');
+
+    const picker = () => {
+        const randomIndex = Math.floor(Math.random() * kodam.length);
+        setRandomItem(kodam[randomIndex]);
+    }
     return ( 
 
-    <div className="bg-indigo-500">
-        <h1 className="text-3xl font-bold">
-        Cek Kodam Kamu
-      </h1>
-      
-
-      
-        <form>
-            <label class="block">
-            <span class="block text-lg font-medium text-slate-700">Masukan Nama Kamu</span>
-            <input class="border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500"/>
-            <p class="mt-2 opacity-50 contrast-more:opacity-100 text-slate-600 text-sm">
-            <button class="bg-cyan-500 hover:bg-cyan-600 ...">Submit</button><br />
-            Kodam kamu adalah: 
-            </p>
-        </label>
-        </form>
-        </div>
+      <div className="App">
+        <header className="App-header">
+        <h1> Cek Kodam Kamu</h1> 
+        <input type="text" placeholder="Masukan Nama kamu" className="input input-bordered w-full max-w-xs mt-2" />
+        <Button className="btn btn-primary mt-3" onClick={picker}>Submit</Button>
+        {randomItem && <p className="mt-3">Kodam kamu adalah: {randomItem}</p>}
+        </header>
+      </div>
      );
 }
  
